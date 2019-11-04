72ae08f1bb4c71ad6452b2dda74872096d1c8b4a
imHarish03 <harishhari302@gmail.com>
2019-11-04 23:40:10 +0530

    Adds entity

---
+++ src/main/java/lab/basic/Hibernate/demo/entity/Course.java
@@ -0,0 +1,58 @@
+package lab.basic.Hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "instructor_id")
+	private String instructorId;
+
+	public Course() {
+
+	}
+
+	public Course(String title, String instructorId) {
+		this.title = title;
+		this.instructorId = instructorId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getInstructorId() {
+		return instructorId;
+	}
+
+	public void setInstructorId(String instructorId) {
+		this.instructorId = instructorId;
+	}
+
+}
